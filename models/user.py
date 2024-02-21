d84a1f74484f8ced4ca66ce7f31a9c5a62cab08a
waltertaya <waltermittybecker5@gmail.com>
2024-02-21 21:14:52 +0300

    Updated: User Class to cater for both file and DB storage

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
+import models
 
 
 class User(BaseModel, Base):
@@ -16,12 +17,23 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    if models.storage_t == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade='all, delete, delete-orphan',
+                              backref="user")
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
