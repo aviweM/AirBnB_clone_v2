a3d55d81f066300b21263cfeed24a26a0618f6a3
waltertaya <waltermittybecker5@gmail.com>
2024-03-24 18:00:21 +0300

    Fixed: Correct output fail

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
